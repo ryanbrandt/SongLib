738ec5d4dbbb8d1e990ebb1d0593e74714bccfca
iansbarry <ian.barry16@gmail.com>
2019-09-22 14:36:10 -0400

    upadate and delete functions

--- src/main/java/store/StoreManager.java
+++ src/main/java/store/StoreManager.java
@@ -32,12 +32,102 @@ public class StoreManager {
     }
 
     public void update(Song oldSong, Song newSong) {
-        // TODO find and update a song
-		//test
+        
+
+        File temp = new File("src/main/java/store/temp.txt");
+
+        try {
+
+            this.writer = new BufferedWriter(new FileWriter(temp, false));
+            this.reader = new BufferedReader(new FileReader(store));
+
+            String storeLine;
+            String[] storeLineArr;
+
+            //make edit in temp
+            while((storeLine = reader.readLine()) != null){
+                storeLineArr = storeLine.split(",");
+
+                String song  =  storeLineArr[0];
+                String artist  =  storeLineArr[1];
+
+                if(song.equalsIgnoreCase(oldSong.getSong()) && artist.equalsIgnoreCase(oldSong.getArtist())){
+                    storeLine = newSong.toString();
+                }
+                this.writer.write(storeLine+"\n");
+            }
+
+            this.close(true);
+            this.close(false);
+
+
+            //copy temp to store
+            this.writer = new BufferedWriter(new FileWriter(store, false));
+            this.reader = new BufferedReader(new FileReader(temp));
+
+            String copyLine;
+
+            while((copyLine = reader.readLine()) != null){
+                this.writer.write(copyLine+"\n");
+            }
+
+        } catch(IOException e ){
+            System.out.println(e);
+        } finally {
+            this.close(true);
+            this.close(false);
+
+            temp.delete();
+        }
     }
 
     public void delete(Song song) {
-        // TODO find and delete a song
+        File temp = new File("src/main/java/store/temp.txt");
+
+        try {
+
+            this.writer = new BufferedWriter(new FileWriter(temp, false));
+            this.reader = new BufferedReader(new FileReader(store));
+
+            String storeLine;
+            String[] storeLineArr;
+
+            //copy all but the song to be deleted over to temp
+            while((storeLine = reader.readLine()) != null){
+                storeLineArr = storeLine.split(",");
+
+                String songCheck  =  storeLineArr[0];
+                String artist  =  storeLineArr[1];
+
+                if(songCheck.equalsIgnoreCase(song.getSong()) && artist.equalsIgnoreCase(song.getArtist())){
+
+                } else {
+                    this.writer.write(storeLine + "\n");
+                }
+            }
+
+            this.close(true);
+            this.close(false);
+
+
+            //copy temp to store
+            this.writer = new BufferedWriter(new FileWriter(store, false));
+            this.reader = new BufferedReader(new FileReader(temp));
+
+            String copyLine;
+
+            while((copyLine = reader.readLine()) != null){
+                this.writer.write(copyLine+"\n");
+            }
+
+        } catch(IOException e ){
+            System.out.println(e);
+        } finally {
+            this.close(true);
+            this.close(false);
+            
+            temp.delete();
+        }
     }
 
     public ObservableList<Song> getSongList() {
