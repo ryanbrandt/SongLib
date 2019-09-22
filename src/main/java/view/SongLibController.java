260f365cb8e56f86c51582e7082004dab6dc7eca
Ryan Brandt <rnb52@scarletmail.rutgers.edu>
2019-09-21 23:21:00 -0400

    finish UI

--- src/main/java/view/SongLibController.java
+++ src/main/java/view/SongLibController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 import store.StoreManager;
@@ -16,7 +17,7 @@ import types.Song;
  */
 public class SongLibController {
     @FXML
-    private TextField song, artist, album, year;
+    private TextField song, artist, album, year, songEdit, artistEdit, albumEdit, yearEdit;
 
     @FXML
     private Text alert;
@@ -40,7 +41,13 @@ public class SongLibController {
         songCol.setCellValueFactory(new PropertyValueFactory<Song, String>("song"));
         songCol.setSortType(TableColumn.SortType.ASCENDING);
         artistCol.setCellValueFactory(new PropertyValueFactory<Song, String>("artist"));
+
+        table.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            if(newSelection != null) this.showSelected(table.getSelectionModel().getSelectedItem());
+        });
+
         this.loadSongs();
+        table.getSelectionModel().selectFirst();
     }
 
     /**
@@ -55,21 +62,25 @@ public class SongLibController {
             this.showAlert("required");
             return;
         }
-
-        Song newSong = new Song(songVal, artistVal);
         final String yearVal = year.getText().trim();
         final String albumVal = album.getText().trim();
 
+        if(!yearVal.isEmpty() && !isNum(yearVal)) {
+            this.showAlert("year");
+            return;
+        }
+
+        Song newSong = new Song(songVal, artistVal);
+
         if (this.storeManager.isUnique(newSong)) {
-            if (!albumVal.isEmpty())
-                newSong.album = albumVal;
-            if (!yearVal.isEmpty())
-                newSong.year = Integer.parseInt(yearVal);
+            if (!albumVal.isEmpty()) newSong.album = albumVal;
+            if (!yearVal.isEmpty()) newSong.year = Integer.parseInt(yearVal);
 
             this.storeManager.put(newSong);
             this.showAlert("success");
-            this.clearForm("add");
+            song.clear(); artist.clear(); album.clear(); year.clear();
             this.loadSongs();
+            table.getSelectionModel().select(newSong);
         } else {
             this.showAlert("unique");
         }
@@ -77,10 +88,6 @@ public class SongLibController {
 
     public void removeSong(ActionEvent e) {
         // TODO call delete and reload
-    }
-
-    public void showEdit(ActionEvent e) {
-        // TODO show edit form
     }
 
     public void editSong(ActionEvent e) {
@@ -95,29 +102,39 @@ public class SongLibController {
         table.getSortOrder().add(songCol);
     }
 
-    private void clearForm(String form) {
-        switch (form) {
-        case "add":
-            song.clear();
-            artist.clear();
-            album.clear();
-            year.clear();
-            break;
-        case "edit":
-            // TODO
+    private boolean isNum(String year) {
+        try {
+            Integer.parseInt(year);
+        } catch (NumberFormatException | NullPointerException e) {
+            return false;
         }
+        return true;
+    }
+
+    private void showSelected(Song selected) {
+        songEdit.setText(selected.song);
+        artistEdit.setText(selected.artist);
+        if(!selected.album.equals("null")) albumEdit.setText(selected.album);
+        if(selected.year != -1) yearEdit.setText(Integer.toString(selected.year));
     }
 
     private void showAlert(String alertType) {
         switch (alertType) {
         case "required":
             alert.setText("Please include a Song Title and Artist Name!");
+            alert.setFill(Color.RED);
             break;
         case "unique":
             alert.setText("Song Title and Artist Name must form a unique pair!");
+            alert.setFill(Color.RED);
             break;
         case "success":
             alert.setText("Song added!");
+            alert.setFill(Color.GREEN);
+            break;
+        case "year":
+            alert.setText("Year must be a number!");
+            alert.setFill(Color.RED);
         }
         alert.setVisible(true);
     }
